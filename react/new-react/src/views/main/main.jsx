07debdf80e60f5a595bab25073de71de9e059f00
chuiniu2019 <577367528@qq.com>
2020-10-08 19:26:01 +0800

    no message

--- react/new-react/src/views/main/main.jsx
+++ react/new-react/src/views/main/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Layout, Menu, Breadcrumb } from 'antd'
 import {
-  // DesktopOutlined,
+  DesktopOutlined,
   // PieChartOutlined,
   // FileOutlined,
   // TeamOutlined,
-  UserOutlined,
+  // UserOutlined,
 } from '@ant-design/icons'
 import { Route } from 'react-router-dom'
 import routers from '@/libs/router'
-import history from 'src/libs/history';
+import history from 'src/libs/history'
 
 const { Header, Content, Footer, Sider } = Layout
 const { SubMenu } = Menu
@@ -21,18 +21,17 @@ export default class SiderDemo extends React.Component {
     let route = routes.find((f) => f.path === location.pathname)
     this.state = {
       collapsed: false,
-      route: route,
-      routers: routers
+      route: route, // 这个是当前显示的组件
+      routers: routers, // 这个是router中的所有的跳转路由
     }
   }
   // state = {
   //   collapsed: false,
   // }
   onCollapse = (collapsed) => {
-    console.log(collapsed)
     this.setState({ collapsed })
   }
-  mapRoute (item) {
+  mapRoute(item) {
     if (item instanceof Array) {
       let re = item.map((it) => {
         return this.mapRoute(it) // 第一次循环就返回了
@@ -42,36 +41,46 @@ export default class SiderDemo extends React.Component {
       if (!item.exact) {
         if (item.routes) {
           return (
-            <SubMenu key={item.path} icon={<UserOutlined />} title={item.name}>
-              {
-                item.routes.map((i) => {
-                  return this.mapRoute(i)
-                })
-              }
+            <SubMenu key={item.path} icon={<DesktopOutlined />} title={item.name}>
+              {item.routes.map((i) => {
+                return this.mapRoute(i)
+              })}
             </SubMenu>
           )
         } else {
-          return <Menu.Item key={item.path} onClick={this.routePush}>{item.name}</Menu.Item>
+          return (
+            // <Menu.Item key={item.path} onClick={this.routePush(item.path)}> // 这个不能传参
+            // onClick={this.handleClick.bind(this, 11, 22)} // 这个也可以传参
+            <Menu.Item key={item.path} onClick={() => this.routePush(item.path)}>
+              {item.name}
+            </Menu.Item>
+          )
         }
       }
     }
   }
-  routePush(){
-    history.push('../beef/sirloin')
+  routePush = (path) => {
+    history.push(`..${path}`) 
+    // 用这个在登陆页面跳转过来是可以直接渲染的,在这里只是改了地址,可能是已经渲染过的缘故!
+    let { routes } = this.props
+    let route = routes.find((f) => f.path === path)
+    this.setState({ // 这个能够让主页面中的页面得到改变.
+      route: route
+    })
   }
-  render () {
-    let abc = this.mapRoute(routers).filter(i => i)
+  render() {
+    let menus = this.mapRoute(routers).filter((i) => i)
 
     return (
       <Layout style={{ minHeight: '100vh' }}>
         <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
-          <div className="logo" />
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            {abc}
+          {/* <div className="logo" /> */}
+          <Menu theme='light' defaultSelectedKeys={['1']} mode="inline">
+            {menus}
           </Menu>
         </Sider>
         <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }} />
+          {/* <Header className="site-layout-background" style={{ padding: 0 }} /> */}
           <Content style={{ margin: '0 16px' }}>
             <Breadcrumb style={{ margin: '16px 0' }}>
               <Breadcrumb.Item>User</Breadcrumb.Item>
@@ -81,7 +90,7 @@ export default class SiderDemo extends React.Component {
               <Route routes={this.state.route} component={this.state.route.component} />
             </div>
           </Content>
-          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+          <Footer style={{ textAlign: 'center' }}>Created By GT</Footer>
         </Layout>
       </Layout>
     )
