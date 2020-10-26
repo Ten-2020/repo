d6c9572cbea092d0212e8414511be9ed633919ba
chuiniu2019 <577367528@qq.com>
2020-10-26 18:46:50 +0800

    no message

--- react/new-react/craco.config.js
+++ react/new-react/craco.config.js
@@ -4,11 +4,12 @@ const CracoLessPlugin = require('craco-less');
 module.exports = {
   devServer: {
     //如果不使用mocker-api库
-    before: require('./mock/api/index')// before如果不行就用after,可惜用不到了
+    before: require('./mock/api/index'),// before如果不行就用after,可惜用不到了
     // before(app){
     //   app.get('/test/get', function(req, res){    
     //    res.json({ get: 'response get' });
     //   });
+    hot:true
   },
   plugins: [
     {
